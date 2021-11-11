fcd3de2238d974ad5922aeb1fe17abc7a629ea0f
Mart <themcman@gmail.com>
2021-11-11 00:25:50 -0600

    redone for one-way

--- src/client/main.rs
+++ src/client/main.rs
@@ -6,42 +6,19 @@ use tokio::{
 
 #[tokio::main]
 async fn main() {
-    let mut stream = TcpStream::connect("localhost:8000").await.unwrap();
-    println!("Connected to {}", &stream.peer_addr().unwrap());
-
-    let (tx, rx) = broadcast::channel::<String>(10);
-    let mut input_buf = [0_u8; 1024];
-    let mut stdin = tokio::io::stdin();
-    let mut network_buf = String::new();
+    let mut stream = TcpStream::connect("127.0.0.1:8000").await.unwrap();
+    let (mut reader, mut writer) = stream.split();
 
     loop {
-        let (reader, mut writer) = stream.split();
-        let mut reader = BufReader::new(reader);
+        let mut input_buf = [0_u8; 10];
+        let mut stdin = tokio::io::stdin();
+        let n = stdin.read(&mut input_buf).await.unwrap();
+        println!("read {:?}", input_buf);
 
-        let tx = tx.clone();
-        let mut rx = tx.subscribe();
-
-        tokio::select! {
-            result = stdin
-                .read(&mut input_buf) => {
-                    if result.unwrap() == 0 {
-                        break;
-                    }
-
-                    let x = std::array::IntoIter::new(input_buf).filter(|&x| x != 0 as u8);
-                    writer.write_all(&input_buf).await.unwrap();
-                println!("wrote {:#?} to network", x.clone());
-                }
-            result = reader.read_line(&mut network_buf) => {
-                println!("read {:#?} on network", network_buf.clone());
-                tx.send(network_buf.clone()).unwrap();
-                network_buf.clear();
-                println!("sent {:#?} on tx", network_buf.clone());
-            }
-            result = rx.recv() => {
-                let msg = result.unwrap();
-                println!("received {:#?} on rx", msg);
-            }
-        }
+        let len = writer.write(&input_buf[..n]).await;
+        println!("written {:?}", len);
+        let mut line = [0_u8]; //String::new();
+        let len = reader.read(&mut line).await;
+        println!("{:?} {:?}", line, len);
     }
 }
--- src/server/main.rs
+++ src/server/main.rs
@@ -1,45 +1,24 @@
-use tokio::{
-    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
-    net::TcpListener,
-    sync::broadcast,
-};
+use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
 
-#[tokio::main]
-async fn main() {
-    let listener: TcpListener = TcpListener::bind("localhost:8000").await.unwrap();
-    let (tx, _rx) = broadcast::channel(10);
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        thread::spawn(move || handle_client(stream));
+    }
+}
 
+fn handle_client(stream: TcpStream) {
+    let mut stream = BufReader::new(stream);
     loop {
-        let (mut socket, addr) = listener.accept().await.unwrap();
-
-        let tx = tx.clone();
-        let mut rx = tx.subscribe();
-
-        tokio::spawn(async move {
-            let (reader, mut writer) = socket.split();
-
-            let mut reader = BufReader::new(reader);
-            let mut line = String::new();
-
-            loop {
-                tokio::select! {
-                        result = reader.read_line(&mut line) => {
-                            if result.unwrap() == 0 {
-                                break;
-                            }
-                            tx.send((line.clone(), addr)).unwrap();
-                            line.clear();
-                        }
-                        result = rx.recv() => {
-                            let (msg, other_addr) = result.unwrap();
-                            println!("{}, {:#?}", msg.len(), &msg);
-                            if addr != other_addr {
-                            writer.write_all(msg.as_bytes()).await.unwrap();
-
-                        }
-                    }
-                }
-            }
-        });
+        let mut buf = String::new();
+        let n = stream.read_line(&mut buf).unwrap();
+        if n == 0 {
+            break;
+        }
+        println!("recieved {:?}", &buf);
+        stream.get_ref().write(buf.as_bytes()).unwrap();
     }
 }
