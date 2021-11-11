cf80894a2ca5682ddd3e9630830418e770495086
Mart <themcman@gmail.com>
2021-11-10 20:43:05 -0600

    client WIP

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,33 +1,47 @@
-// use std::io::BufRead;
-use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader, Stdin};
-use tokio::net::TcpStream;
+use tokio::{
+    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
+    net::TcpStream,
+    sync::broadcast,
+};
 
 #[tokio::main]
 async fn main() {
-    let mut stream = TcpStream::connect("localhost:8000")
-        .await
-        .expect("Could not connect.");
+    let mut stream = TcpStream::connect("localhost:8000").await.unwrap();
+    println!("Connected to {}", &stream.peer_addr().unwrap());
+
+    let (tx, rx) = broadcast::channel::<String>(10);
+    let mut input_buf = [0_u8; 1024];
+    let mut stdin = tokio::io::stdin();
+    let mut network_buf = String::new();
+
     loop {
-        let mut input = Vec::new();
-        // let mut buffer = String::new();
-        tokio::io::stdin()
-            .read_exact(&mut input)
-            .await
-            .expect("Failed to read from stdin");
-        stream
-            .write(&input)
-            .await
-            .expect("Failed to write to server");
+        let (reader, mut writer) = stream.split();
+        let mut reader = BufReader::new(reader);
 
-        // let mut reader = BufReader::new(stream);
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
-        // reader
-        //     .read_line(&mut buffer)
-        //     .await
-        //     .expect("Could not read into buffer");
-        // print!(
-        //     "{}",
-        //     std::str::from_utf8(&buffer).expect("Could not write buffer as string")
-        // );
+        tokio::select! {
+            result = stdin
+                .read(&mut input_buf) => {
+                    if result.unwrap() == 0 {
+                        break;
+                    }
+
+                    let x = std::array::IntoIter::new(input_buf).filter(|&x| x != 0 as u8);
+                    writer.write_all(&input_buf).await.unwrap();
+                println!("wrote {:#?} to network", x.clone());
+                }
+            result = reader.read_line(&mut network_buf) => {
+                println!("read {:#?} on network", network_buf.clone());
+                tx.send(network_buf.clone()).unwrap();
+                network_buf.clear();
+                println!("sent {:#?} on tx", network_buf.clone());
+            }
+            result = rx.recv() => {
+                let msg = result.unwrap();
+                println!("received {:#?} on rx", msg);
+            }
+        }
     }
 }
